c618dad699e68da314a609da426c5bf1acdbda28
Sara <sarabeytuie@gmail.com>
2025-07-25 17:13:19 +0330

    feat(pinia-second): add base router and pages

--- pinia-second/src/router/index.js
+++ pinia-second/src/router/index.js
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: () => import('@/views/auth/LoginPage.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/admin',
+      name: 'admin',
+      component: () => import('@views/admin/IndexPage.vue'),
+      children: [
+        {
+          path: '/',
+          name: '',
+          component: () => import('@/views/admin/pages/DashboardPage.vue'),
+        },
+        {
+          path: '/posts',
+          name: 'posts',
+          component: () => import('@/views/admin/pages/PostPage.vue'),
+        },
+      ],
     },
   ],
 })
